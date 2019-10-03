499e6fcc19e76d1abba181dda30e6dcb5577c538
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-10-02 22:02:15 -0400

    Done with part 1. working!

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter as Router} from 'react-router-dom'; 
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
-ReactDOM.render(
-<Router>
+ReactDOM.render(<Router>
   <App />
 </Router>, document.getElementById('root'));
